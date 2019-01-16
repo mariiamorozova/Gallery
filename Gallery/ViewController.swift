d4f20e9dfd80dc9cbed6d8f65bce7f34960da98c
Mariia Morozova <mariia.morozova@qb.studio>
2019-01-16 12:58:14 +0300

    Created project.

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Mariia Morozova on 16/01/2019.
+//  Copyright © 2019 Mariia Morozova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
